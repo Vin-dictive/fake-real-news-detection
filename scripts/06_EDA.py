3b09923985a480f0367968e8f23e0967e959ba8b
Jessie Liang <rnliang.jessie@gmail.com>
2025-12-03 22:24:20 -0800

    fake_real_count.png script done

--- scripts/06_EDA.py
+++ scripts/06_EDA.py
@@ -6,6 +6,7 @@ import click
 import os
 import pandas as pd
 from io import StringIO
+import matplotlib.pyplot as plt
 
 # terminal command to run script:
 # python scripts/06_EDA.py \
@@ -21,13 +22,25 @@ def main(train_data_path):
         # Load train dataset
         train_df = load_data(train_data_path)
 
-        # train_df information
+        # train_df.info() as text file
         buffer = StringIO()
         train_df.info(buf=buffer)
         os.makedirs("data/text", exist_ok=True)
         with open("data/text/train_df_info.txt", "w") as f:
             f.write(buffer.getvalue())
         print("Successfully saved train_df info to data/text as a text file")
+
+        # plot: Count of Fake vs. Real News Articles
+        counts = train_df['target'].value_counts().sort_index()
+        plt.figure(figsize=(6, 1.5)) 
+        plt.barh(counts.index, counts.values)
+        plt.xlabel('Count')
+        plt.ylabel('Label')
+        plt.title('Count of true and fake news articles in training data')
+        fig_path_1 = "img/fake_real_count.png"
+        plt.tight_layout()
+        plt.savefig(fig_path_1, dpi=300, bbox_inches="tight")
+        print("Successfully saved fake_real_count.png to img/ folder")
 
     except FileNotFoundError as e:
         print(f"File not found: {e.filename}")
